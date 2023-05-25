b02706233218b02dd4f4b503eeef03e69f0e28d2
Marcelo02 <90460968+Marcelo02@users.noreply.github.com>
2023-05-25 08:52:28 -0300

    Add files via upload

---
+++ player1.py
@@ -0,0 +1,19 @@
+import pika
+
+url = 'amqps://hgglhdrq:PCCHVDRLB-NiZeJyAiHwnmu5dNUliGNg@jackal.rmq.cloudamqp.com/hgglhdrq'
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='fila')
+
+
+def send():
+    move = input("Enter your move (rock, paper, or scissors): ").lower()
+    while move not in ['rock', 'paper', 'scissors']:
+        print("Invalid move. Please try again.")
+        move = input("Enter your move (rock, paper, or scissors): ").lower()
+    channel.basic_publish(exchange='', routing_key='fila', body=move)
+    print('Sent:', move)
+
+
+send()
---
+++ player2.py
@@ -0,0 +1,45 @@
+import pika
+
+url = 'amqps://hgglhdrq:PCCHVDRLB-NiZeJyAiHwnmu5dNUliGNg@jackal.rmq.cloudamqp.com/hgglhdrq'
+params = pika.URLParameters(url)
+connection = pika.BlockingConnection(params)
+channel = connection.channel()
+channel.queue_declare(queue='fila')
+
+
+def callback(ch, method, properties, body):
+    p1_move = body.decode()
+    p2_move = get_player_move()
+    result = determine_winner(p1_move, p2_move)
+    print(result)
+
+
+def get_player_move():
+    while True:
+        move = input("Enter your move (rock, paper, or scissors): ").lower()
+        if move in ['rock', 'paper', 'scissors']:
+            return move
+        else:
+            print("Invalid move. Please try again.")
+
+
+def determine_winner(p1_move, p2_move):
+    if p1_move == p2_move:
+        return "It's a tie!"
+    elif (p1_move == 'rock' and p2_move == 'scissors') or \
+            (p1_move == 'paper' and p2_move == 'rock') or \
+            (p1_move == 'scissors' and p2_move == 'paper'):
+        return "Player 1 wins!"
+    else:
+        return "Player 2 wins!"
+
+
+def play_game():
+    print("Welcome to Rock, Paper, Scissors!")
+
+    channel.basic_consume(queue='fila', on_message_callback=callback, auto_ack=True)
+    print("Waiting for opponent...")
+    channel.start_consuming()
+
+
+play_game()
